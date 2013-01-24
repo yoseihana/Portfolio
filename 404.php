b73df433c19516a5cbc4565c7e97a8fd7eef048f
Annabelle Buffart <anna.buffart@gmail.com>
2013-01-24 18:48:53 +0100

    RWD ok
    TODO: - Validation
    - checklist
    - formulaire

--- 404.php
+++ 404.php
@@ -8,13 +8,16 @@
 						
 							<header class="article-header">
 							
-								<h1>Erreur 404 - Cette page n'existe pas</h1>
+								<h1>Oops un problème</h1>
 						
 							</header> <!-- end article header -->
 					
 							<section class="entry-content">
-							
-								<p>Oups! Il y a eu un problème dans votre demande, la page demandée n'existe pas.</p>
+							    <figure>
+                                    <img src="http://anna.buffart.eu/wp-content/uploads/2013/01/404.jpg" title="Un gentil chat pour ne pas être triste" alt="Photo d'un gentil chat pour ne pas être triste" />
+							    </figure>
+								<p>Oups! Il y a eu un problème dans votre demande, la page demandée n'existe pas. Mais cadeau! La photo d'un chat!</p>
+                                <p>Perdu? Pas de panique, <a href="http://anna.buffart.eu/" title="Retour vers la page d'accueil du site">cliquez vite ici pour retourner sur la page d'accueil!</a></p>
 					
 							</section> <!-- end article section -->
 					
--- archive.php
+++ archive.php
@@ -48,18 +48,14 @@
 					    <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 						
 						    <section>
-							    <header class="article-header">
-								
-								   <!-- <h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>-->
-								   <h1><?php the_title(); ?></h1>
-								
-								    <p class="byline vcard"><?php _e("Posted", "bonestheme"); ?> <time class="updated" datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by", "bonestheme"); ?> <span class="author"><?php the_author_posts_link(); ?></span> <span class="amp">&</span> <?php _e("filed under", "bonestheme"); ?> <?php the_category(', '); ?>.</p>
-							
+							    <header class="posted article-header">
+								   <h2><?php the_title(); ?></h2>
+
+								    <p class="byline vcard">Posté le <time class="updated" datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> par <span class="author"><?php the_author_posts_link(); ?></span> dans la catégorie <?php the_category(', '); ?>.</p>
+                                    </header>
 							    </header> <!-- end article header -->
 						
 							    <section class="entry-content clearfix">
-							
-								    <!--<?php the_post_thumbnail( 'bones-thumb-300' ); ?>-->
 								    <?php the_post_thumbnail( 'medium' ); ?>
 							
 								    <?php the_content(); ?>
